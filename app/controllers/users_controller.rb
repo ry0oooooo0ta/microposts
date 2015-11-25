58e1e23ba4ccd7bc2b4856986242f251830df056
ry0oooooo0ta <ryota_s@r.recruit.co.jp>
2015-11-25 15:58:42 +0000

    7-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def show
     @user = User.find(params[:id])
   end
@@ -15,6 +14,19 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user =User.find(params[:id])
+  end
+  
+  def update #users/:id method=patch
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
   
